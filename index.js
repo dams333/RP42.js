af5ad13c0b2aea6950ba32905e0c55fdd2986da5
dhubleur <dhubleur@student.42.fr>
2023-02-15 16:58:46 +0100

    feat(COA): Add script to generate basic coalition image

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const client = require('discord-rich-presence')("1075433460275101696");
 
-client.updatePresence({
-	state: 'slithering',
-	details: '🐍',
-	startTimestamp: Date.now(),
-	endTimestamp: Date.now() + 1337,
-	largeImageKey: 'snek_large',
-	smallImageKey: 'snek_small',
-	instance: true,
-  });
+function updatePresence(login, lvl, location, campus, coalition_logo_key, startedAt) {
+	client.updatePresence({
+		largeImageKey: "rp42-icon",
+		smallImageKey: coalition_logo_key,
+		details: `${login} | Lvl ${lvl}%`,
+		state: `${location} in ${campus}`,
+		startTimestamp: startedAt,
+		instance: true,
+	});
+}
+
+updatePresence("dhubleur", 9.23, "e1r1p1", "Paris", 'coa-order', Date.now());
---
+++ scripts/create_coalition_icon.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import requests
+from cairosvg import svg2png
+from io import BytesIO
+
+
+def create_icon(coa_logo_url, coa_background_color, output):
+	result = Image.new('RGB', (512, 512), color = coa_background_color)
+
+	coa_svg = requests.get(coa_logo_url).content
+	coa_png = svg2png(bytestring=coa_svg)
+	logo = Image.open(BytesIO(coa_png))
+
+	logo = logo.convert('RGBA')
+	logo = logo.resize((470, 470))
+	result.paste(logo, (21, 21), logo)
+
+	result.save(output)
+
+create_icon('https://cdn.intra.42.fr/coalition/image/47/order.svg', '#FF6950', 'assets/coa-order.png')
